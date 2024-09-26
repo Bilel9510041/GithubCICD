2be833cd58a620e80b1a8e01f56b2acd8c4dc108
Bilel9510041 <bilelmoumni101@gmail.com>
2024-09-26 17:42:25 +0800

    Add initial FastAPI workflow

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
-import json, os
-from pydantic import BaseModel
-from typing import List, Optional, Dict, Any
 
-# Initialize FastAPI app
 app = FastAPI()
 
 @app.get("/")
-async def test():
-    return {"message": "OK"}
+def read_root():
+    return {"Hello": "World"}
